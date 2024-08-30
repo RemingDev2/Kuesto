95b028eb7a1f0d932a4c223826e1eaf6df4f003e
RemingDev <remingdev@gmail.com>
2024-08-30 20:21:28 +0200

    Add DataXLSXtraction class for data extraction from Excel
    Introduced the DataXLSXtraction class, including its headers and implementation files. Updated CMakeLists.txt to include the new files and ensured proper linkage with existing project components. This class provides functionality to extract and retrieve data in the form of a nested map.

--- src/files/excel/DataXLSXtraction.cpp
+++ src/files/excel/DataXLSXtraction.cpp
@@ -1,8 +1,19 @@
 #include "../../headers/excel/DataXLSXtraction.h"
 #include "../../../external/tinyxml2/tinyxml2.h"
+#include <filesystem>
+
 
 void DataXLSXtraction::extractData()
 {
+    std::string tempDirPath = "temp/extracted_sheets"; // chemin vers le dossier temporaire
+
+//    for (const auto& sheet : std::filesystem::directory_iterator(tempDirPath)) // pour chaque feuille
+//    {
+//
+//    }
+    tinyxml2::XMLDocument sheetFile; // init objet pour contenir feuille
+    std::string sheetPath= tempDirPath + "/sheet1.xml"; // donnée brut pour test
+    sheetFile.LoadFile(sheetPath.c_str()); // chargement xml dans objet
 
 }
 
